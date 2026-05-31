f3cb2d4887b8daac0f8acf56c414bf56c06a65f7
MonsterLuni <monsterluni@gmail.com>
2026-05-31 10:14:26 +0200

    refactor: made number appear rightfully

--- Frontend/numbergenerator/src/app/numbervault/numbervault.ts
+++ Frontend/numbergenerator/src/app/numbervault/numbervault.ts
@@ -4,18 +4,28 @@ import { NumberGeneratorService } from '../../services/number-generator.service'
 import { NumberfieldComponent } from '../numberfield/numberfield';
 import { GeneratedNumberDto } from '../../dtos/generated-number.dto';
 import { CreateGeneratedNumberDto } from '../../dtos/create-generated-number.dto';
+import { form, FormField, max, min } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-numbervault-component',
-  imports: [NavbarComponent, NumberfieldComponent],
+  imports: [NavbarComponent, NumberfieldComponent, FormField],
   templateUrl: './numbervault.html',
   styleUrl: './numbervault.css',
 })
 export class NumbervaultComponent implements OnInit {
+  protected numbergeneratorForm = form(signal({
+    digits: 5
+  }),(schemaPath) => {
+    min(schemaPath.digits, 1, {message: 'Digits must be at least 1'});
+    max(schemaPath.digits, 7, {message: 'Digits must be at most 7'});
+  });
   protected amount = 12;
   protected randomNumber = signal(0);
   protected loadingNumber = false;
-  protected number?: number;
+
+  protected number(){
+    return Math.pow(10,this.numbergeneratorForm().controlValue().digits) - 1;
+  }
   protected numbers = signal<GeneratedNumberDto[]>([]);
 
   public constructor(private readonly numberGeneratoService: NumberGeneratorService) {}
@@ -41,10 +51,10 @@ export class NumbervaultComponent implements OnInit {
 
     var generatedNumberDto = await this.numberGeneratoService.postAsync({
       userId: '123e4567-e89b-12d3-a456-426655440000',
-      min: 50,
-      max: 100,
+      min: Math.pow(10, this.numbergeneratorForm().controlValue().digits - 1),
+      max: Math.pow(10, this.numbergeneratorForm().controlValue().digits) - 1,
     } as CreateGeneratedNumberDto);
-    this.number = generatedNumberDto.value;
+    //this.number = generatedNumberDto.value;
 
     await this.delay(1000);
 
@@ -56,7 +66,7 @@ export class NumbervaultComponent implements OnInit {
 
   private startRandomNumberAnimation(): number {
     const intervalId = setInterval(() => {
-      this.randomNumber.set(Math.round(Math.random() * 1000));
+      this.randomNumber.set(Math.pow(10, this.numbergeneratorForm().controlValue().digits - 1) + Math.round(Math.random() * (Math.pow(10, this.numbergeneratorForm().controlValue().digits) - Math.pow(10, this.numbergeneratorForm().controlValue().digits - 1) - 1)));
     }, 100);
     return intervalId;
   }
